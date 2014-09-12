a3cd703444d85e8ff24873250eaa53a3b14b2527
Joshua Chaitin-Pollak <josh@offthehill.org>
2014-09-12 11:35:46 -0400

    fixed CPP test and package

--- manifests/cpp.pp
+++ manifests/cpp.pp
@@ -3,13 +3,18 @@
 # Examples
 #
 #   include eclipse::cpp
+#   class { 'eclipse::cpp':
+#     release => 'luna',
+#     version => 'R'
+#    }
+#
 class eclipse::cpp(
   $release='luna',
   $version='R',
   $mirror='http://ftp.osuosl.org/pub/eclipse') {
 
 
-  package { 'Eclipse-CPP-${release}-${version}':
+  package { "Eclipse-CPP-${release}-${version}":
     provider => 'compressed_app',
     source   => "${mirror}/technology/epp/downloads/release/${release}/${version}/eclipse-cpp-${release}-${version}-macosx-cocoa-x86_64.tar.gz",
   }
--- spec/classes/eclipse_cpp_spec.rb
+++ spec/classes/eclipse_cpp_spec.rb
@@ -4,7 +4,7 @@ describe 'eclipse::cpp' do
   it do
     should contain_package('Eclipse-CPP-luna-R').with({
       :provider => 'compressed_app',
-      :source   => 'http://ftp.osuosl.org/pub/eclipse/epp/downloads/release/luna/R/eclipse-cpp-luna-R-macosx-cocoa-x86_64.tar.gz',
+      :source   => 'http://ftp.osuosl.org/pub/eclipse/technology/epp/downloads/release/luna/R/eclipse-cpp-luna-R-macosx-cocoa-x86_64.tar.gz',
     })
   end
 end
